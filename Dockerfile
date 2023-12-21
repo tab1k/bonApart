eb7e1ca2f347916b483bbe87e96beaabc296601e
Tabik Karbayev <tabigatkarbayev@icloud.com>
2023-12-21 20:48:33 +0600

    letsencrypt

--- Dockerfile
+++ Dockerfile
@@ -15,10 +15,10 @@ RUN pip3 install --upgrade pip
 RUN pip3 install -r requirements.txt
 RUN pip3 install Pillow psycopg2
 
-
 COPY . /code
 COPY ./docker-entrypoint.sh ./docker-entrypoint.sh
 
-# Set docker-entrypoint
 RUN chmod +x /code/docker-entrypoint.sh
 CMD ["/code/docker-entrypoint.sh"]
+
+
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,1 +1,17 @@
-python manage.py collectstatic --noinput
+#!/bin/bash
+set -e
+
+# Ждем запуска PostgreSQL
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "db" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
+  >&2 echo "PostgreSQL is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "PostgreSQL is up - continuing with Django entrypoint"
+
+# Применяем миграции
+python manage.py migrate
+python manage.py collectstatic
+
+# Запускаем сервер
+exec "$@"

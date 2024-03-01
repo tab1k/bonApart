e25f7078f304d2d66dd2e4c07ad3845750b8dffc
Карбаев Табигат <tabigat@MacBook-Pro-Karbaev.local>
2024-03-01 16:52:26 +0500

    adding phone_num

--- bonapart_project/settings.py
+++ bonapart_project/settings.py
@@ -80,18 +80,18 @@ WSGI_APPLICATION = "bonapart_project.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
     # "default": {
-    #         "ENGINE": "django.db.backends.postgresql_psycopg2",
-    #         'NAME': 'bonapart_db',
-    #         'USER': 'tab1k',
-    #         'PASSWORD': 'TOBI8585',
-    #         'HOST': 'db',
-    #         'PORT': '5432',
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
     # }
+    "default": {
+            "ENGINE": "django.db.backends.postgresql_psycopg2",
+            'NAME': 'bonapart_db',
+            'USER': 'tab1k',
+            'PASSWORD': 'TOBI8585',
+            'HOST': 'db',
+            'PORT': '5432',
+    }
 }
 
 
